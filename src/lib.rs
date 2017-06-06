e78a4782c10d742264a6b15ada3e7be1347b06b1
Austin Hicks <camlorn@camlorn.net>
2017-06-06 14:14:17 -0400

    Reimplement on top of an abstraction which allows nodes to load lazily.

--- src/lib.rs
+++ src/lib.rs
@@ -2,132 +2,184 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate bincode;
+extern crate take_mut;
 
 use std::io;
+use std::error::Error;
 use std::marker::PhantomData;
 
-#[derive(Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd, Debug, Clone, Copy)]
+
+#[derive(Debug)]
+pub enum DecodingError {
+    Corrupt(String),
+    IoError(io::Error),
+}
+
+fn decode<O: serde::de::DeserializeOwned, R: io::Read+io::Seek>(reader: &mut R, offset: u64) -> Result<O, DecodingError> {
+    reader.seek(io::SeekFrom::Start(offset)).map_err(|x| DecodingError::IoError(x))?;
+    bincode::deserialize_from(    reader, bincode::Infinite)
+    .map_err(|x| {
+        match *x {
+            bincode::ErrorKind::IoError(y) => DecodingError::IoError(y),
+            _ => DecodingError::Corrupt(x.description().to_string()),
+        }
+    })
+}
+
+#[derive(Debug)]
+pub enum EncodingError {
+    Unknown(String),
+    IoError(io::Error),
+}
+
+fn encode<O: serde::Serialize, W: io::Write+io::Seek>(writer: &mut W, obj: &O) -> Result<u64, EncodingError> {
+    let offset = writer.seek(io::SeekFrom::End(0)).map_err(|x| EncodingError::IoError(x))?;
+    bincode::serialize_into(writer, obj, bincode::Infinite)
+    .map_err(|x| {
+        match *x {
+            bincode::ErrorKind::IoError(y) => EncodingError::IoError(y),
+            _ => EncodingError::Unknown(x.description().to_string()),
+        }
+    }).map(|_| offset)
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Ord, PartialOrd, Eq, PartialEq)]
 enum NodeType {
+    Root,
     Internal,
-    Leaf,
+    Leaf
 }
 
-impl Default for NodeType {
-    fn default() -> NodeType { NodeType::Internal }
+enum NodeRef<K> {
+    Unloaded(u64),
+    Loaded(Box<Node<K>>),
+    Modified(Box<Node<K>>),
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+// An on-disk representation, for space saving.
+#[derive(Serialize, Deserialize)]
+struct DiskNode<K> {
+    node_type: NodeType,
+    children: Vec<(K, u64)>,
+    after_last: u64,
+}
+
 struct Node<K> {
     node_type: NodeType,
-    // Each of these is the largest key found in the subtree.
-    children: Vec<(K, u64)>,
-    final_child: u64,
-    #[serde(skip)]
-    file_offset: Option<u64>,
+    children: Vec<(K, NodeRef<K>)>,
+    after_last: NodeRef<K>,
 }
 
-impl<K: Copy+Ord+serde::de::DeserializeOwned> Node<K> {
+impl<K> From<DiskNode<K>> for Node<K> {
+    fn from(obj: DiskNode<K>) -> Node<K> {
+        Node {
+            node_type: obj.node_type,
+            children: obj.children.into_iter().map(|x| (x.0, NodeRef::Unloaded(x.1))).collect(),
+            after_last: NodeRef::Unloaded(obj.after_last),
+        }
+    }
+}
 
-    fn from_reader<R: io::Read+io::Seek>(reader: &mut R, offset: u64) -> bincode::Result<Node<K>> {
-        reader.seek(io::SeekFrom::Start(offset))
-        .map_err(
-            |x| Box::new(bincode::internal::ErrorKind::IoError(x))
-        )?;
-        let mut res: Node<K> = bincode::deserialize_from(reader, bincode::Infinite)?;
-        res.file_offset = Some(offset);
-        Ok(res)
+impl<K: serde::de::DeserializeOwned> DiskNode<K> {
+    fn load<R: io::Read+io::Seek>(reader: &mut R, offset: u64) -> Result<DiskNode<K>, DecodingError> {
+        decode(reader, offset)        
+    }
+}
+
+impl<K: serde::de::DeserializeOwned> NodeRef<K> {
+
+    fn load<R: io::Read+io::Seek>(reader: &mut R, offset: u64) -> Result<Node<K>, DecodingError> {
+        Ok(DiskNode::<K>::load(reader, offset)?.into())
     }
 
-
-    /// Load and return leaf node which may contain the given key.
-    fn find_leaf<R: io::Read+io::Seek>(&self, reader: &mut R, key: &K) -> bincode::Result<Node<K>> {
-        if self.node_type == NodeType::Leaf { return Ok(self.clone()); }
-        // The traditional btree algorithm finds the first key greater than the one we want, then returns the one before it.
-        // Our nodes will be quite large, so it's better to binary search.
-        let found = self.children.binary_search_by_key(&key, |x| &x.0);
-        let child_offset = match found {
-            Ok(index) => {
-                // The keys in the vec are the highest key in the subtree. If we exactly match, it's this subtree.
-                self.children[index].1
-            }
-            Err(index) => {
-                // Otherwise, we get an index between two subtrees.
-                // The key is greater than the element this index points at, but the vec is storing maximums.
-                let wanted = index+1;
-                self.children.get(wanted).map(|x| x.1).unwrap_or(self.final_child)
-            }
+    // Upgrade through the variants, optionally all the way to mutable.
+    fn upgrade<R: io::Read+io::Seek>(&mut self, reader: &mut R, modification: bool) -> Result<(), DecodingError> {
+        let new: Option<Box<Node<K>>> = match *self {
+            NodeRef::Unloaded(offset) => Some(Box::new(NodeRef::load(reader, offset)?)),
+            _ => None,
         };
-        // Load and recurse.
-        let n = Node::from_reader(reader, child_offset)?;
-        n.find_leaf(reader, key)
-    }
-
-    /// Does this subtree contain the key? If so, what's the offset?
-    fn offset_for_key<R: io::Read+io::Seek>(&self, reader: &mut R, key: &K) -> bincode::Result<Option<u64>> {
-        if self.node_type == NodeType::Leaf {
-            Ok(self.scan(key))
-        } else {
-            Ok(self.find_leaf(reader, key)?.scan(key))
+        // If we have a new one, construct the appropriate variant directly.
+        if let Some(b)  = new {
+            if modification {
+                *self = NodeRef::Modified(b);
+            } else {
+                *self = NodeRef::Loaded(b);
+            }
+            return Ok(());
         }
-    }
-
-    fn scan(&self, key: &K) -> Option<u64> {
-        assert_eq!(self.node_type, NodeType::Leaf);
-        if let Ok(ind) = self.children.binary_search_by_key(key, |x| x.0) {
-            Some(self.children[ind].1)
-        } else {
-            None
+        // If we get here, it's either Loaded or Modified, with a possible need for an upgrade.
+        if modification {
+            take_mut::take(self, |x| match x {
+                NodeRef::Loaded(b) | NodeRef::Modified(b) => NodeRef::Modified(b),
+                _ => panic!("The node should be loaded."),
+            });
         }
+        Ok(())
+    }
+
+    fn examine<R: io::Read+io::Seek>(&mut self, reader: &mut R) -> Result<&mut Node<K>, DecodingError> {
+        self.upgrade(reader, false)?;
+        Ok(match *self {
+            NodeRef::Loaded(ref mut n) => n,
+            NodeRef::Modified(ref mut n) => n,
+            _ => panic!("Nodes should be loaded."),
+        })
+    }
+
+    fn modify<R: io::Read+io::Seek>(&mut self, reader: &mut R) -> Result<&mut Node<K>, DecodingError> {
+        self.upgrade(reader, true);
+        Ok(match *self {
+            NodeRef::Modified(ref mut n) => n,
+            _ => panic!("Nodes should be modified."),
+        })
     }
 }
 
-/** A B+ tree backed by some storage method.
-
-Internally, this B+ tree uses a log file format, and only ever appends.  All modifying operations return a new BPTree.  All previous versions of the tree remain valid.
-
-In order to make this work, B+ trees do not take references to their readers.  Instead, all functions take a reference.  Always use the same one save for compacting, in which case the destination reader is the reader associated with the tree.
-
-Two BPTrees are equivalent if they have the same offset and are ordered by offset in the backing storage.
-
-It is safe for multiple trees to share the same backing storage.  It is safe to write arbitrary data to the backing storage.
-
-The serde-serialized representation of a BPTree is the offset in the backing storage for the tree.  You will need to keep the backing storage.  This exists to make use in headers more convenient, not for general storage.*/
-#[derive(Default, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
-pub struct BPTree<K, V> {
-    offset: u64,
-    _phantom: PhantomData<(K, V)>,
-}
-
-impl<K, V> BPTree<K, V>
-where K: Copy+Ord+serde::Serialize+serde::de::DeserializeOwned,
-V: Copy+Ord+serde::Serialize+serde::de::DeserializeOwned, {
-    pub fn new() -> BPTree<K, V> {
-        BPTree::from_offset(0)
-    }
-
-    pub fn from_offset(offset: u64) -> BPTree<K, V> {
-        BPTree { offset, _phantom: Default::default() }
-    }
-
-    pub fn contains<R: io::Read+io::Seek>(&self, reader: &mut R, key: &K) -> bincode::Result<bool> {
-        let root = self.get_root(reader)?;
-        Ok(root.offset_for_key(reader, key)?.is_some())
-    }
-
-    fn get_root<R: io::Read+io::Seek>(&self, reader: &mut R) -> bincode::Result<Node<K>> {
-        Node::from_reader(reader, self.offset)
+impl<K: serde::de::DeserializeOwned+Eq+Ord> Node<K> {
+    fn find_offset_for<R: io::Read+io::Seek>(&mut self, reader: &mut R, key: &K) -> Result<Option<u64>, DecodingError> {
+        // If we're the root and there are no children, this is the empty tree.
+        if self.node_type == NodeType::Root && self.children.len() == 0 { return Ok(None) }
+        let index = self.children.binary_search_by_key(&key, |x| &x.0);
+        let mut reference;
+        match index {
+            Ok(ind) if self.node_type == NodeType::Leaf => {
+                let r = &self.children[ind].1;
+                match *r {
+                    NodeRef::Unloaded(offset) => return Ok(Some(offset)),
+                    _ => panic!("Tree contains leaf inside leaf."),
+                }
+            }
+            // The right subtree is >, the left subtree is <=.
+            // The vector stores the offset of the left subtree.
+            Ok(ind) => {
+                reference = &mut self.children[ind].1;
+            }
+            Err(ind) => {
+                // Recall that empty nodes are not possible.
+                if ind < self.children.len() {
+                    reference = &mut self.children[ind].1;
+                } else {
+                    reference = &mut self.after_last;
+                }
+            }
+        }
+        reference.examine(reader)?.find_offset_for(reader, key)
     }
 }
 
-impl<K, V> serde::Serialize for BPTree<K, V> {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        self.offset.serialize(serializer)
-    }
+
+struct BPTree<'a, R: 'a, K, V> {
+    root_reference: NodeRef<K>,
+    backing_io: &'a mut R,
+    _phantom: PhantomData<V>
 }
 
-impl<'de, K, V> serde::Deserialize<'de> for BPTree<K, V> {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let offset = <u64 as serde::Deserialize<'de>>::deserialize(deserializer)?;
-        Ok(BPTree { offset, _phantom: Default::default() })
+impl<'a, R: io::Read+io::Seek, K: serde::de::DeserializeOwned+Eq+Ord, V> BPTree<'a, R, K, V> {
+    pub fn contains(&mut self, key: &K) -> Result<bool, DecodingError> {
+        Ok(self.offset_for(key)?.is_some())
+    }
+
+    pub fn offset_for(&mut self, key: &K) -> Result<Option<u64>, DecodingError> {
+        self.root_reference.examine(self.backing_io)?.find_offset_for(self.backing_io, key)
     }
 }
